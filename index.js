96c36966368ac406a42f5585d05ec60d70d4f18f
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-04-01 01:27:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+
+  ]
+
+   function  superbowlWin(record){
+
+    let winningRecord = record.find(game => game.result === "W");
+      if (winningRecord){
+        return winningRecord.year;
+
+      }else {
+        return undefined;
+      }
+   }
+  
